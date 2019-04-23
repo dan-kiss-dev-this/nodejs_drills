955824b098ddd9601ffaa2823da3d599ef53e2f9
Dan Kiss <flowfast47@gmail.com>
2019-04-22 20:00:18 -0700

    see how primative vs object points to memory by looking passing a value vs passing a location to a reference in memory

---
+++ D6_ByValueByReference/Starter/app.js
@@ -0,0 +1,22 @@
+//pass by value
+function change(b) {
+  b=2;
+};
+
+var a =1
+
+console.log(a);
+
+//pas by reference
+function changeObj(d) {
+  d.prop1= function() {};
+  d.prop2={};
+}
+
+var c = {};
+
+changeObj(c);
+
+//as c is referened and d tell it what the reference should look like, c will change as you modify it in memory
+//to an object you can add in and change properties due to ability to change by reference
+console.log(c);
