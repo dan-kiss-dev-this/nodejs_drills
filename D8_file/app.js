a908a82b894ce3d378ae82667f08c14768bd68cb
Dan Kiss <flowfast47@gmail.com>
2019-04-29 21:36:56 -0700

    restructure folders to use json

---
+++ D8_file/app.js
@@ -0,0 +1,6 @@
+var greet = require('./greet');
+
+//note you are grabbing the greet folder with has the english() and spanish() methods
+
+greet.english();
+greet.spanish();
---
+++ D8_file/greet/english.js
@@ -0,0 +1,7 @@
+var greetings = require('./greetings.json.js');
+
+var greet = function() {
+  console.log(greetings.en);
+}
+
+module.exports = greet;
