ffd9e1a6bf4e72681b2956cef089c30ff6fefec1
Dan Kiss <flowfast47@gmail.com>
2019-05-02 16:11:05 -0700

    set up way to pipe data

--- F11_Pipes/Starter/app.js
+++ F11_Pipes/Starter/app.js
@@ -1,10 +1,17 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
 var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
 
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function(chunk) {
-	console.log(chunk);
-	writable.write(chunk);
-});
+var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+var gzip = zlib.createGzip();
+
+// readable is source and writeable is destination
+readable.pipe(writable);
+
+// gzip is readable so it also has a .pipe method, we take a readable pipe it to gzip and take that compressed output and sends compressed output to  greet.txt.gz
+//this is an example of method chaning, what happens is the method returns an object so we can keep calling methods
+readable.pipe(gzip).pipe(compressed);
